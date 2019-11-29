89fafe34eff72403113fd072ad9204f54d6ef2ee
Beatriz Sato <beatriz1.sato@gmail.com>
2019-11-29 00:40:17 -0200

    jogo pronto

--- ProjetoIntegrador/src/Apocalipse/apocalipse.java
+++ ProjetoIntegrador/src/Apocalipse/apocalipse.java
@@ -78,10 +78,15 @@ public class apocalipse {
         return num;
     }
     
+    static void imprimirStatus(int saude, String arma){
+        System.out.println("Status atual: ");
+        System.out.println("Saúde: "+saude);
+        System.out.println("Arma: "+ arma);
+    }
     public static void main(String[] args) {
-        String arma = "machado";
+        String arma = "não tem";
         char op;
-        int saude = 3, pontos = 0, saudeT, pontosT, num;
+        int saude = 5, pontos = 0, saudeT, pontosT, num;
         boolean amigo = false, deNovo = false, morreu = false,
                 deNovoJogo = true;
         
@@ -108,8 +113,60 @@ public class apocalipse {
             + "campus está totalmente abandonado."};
         
         //BLOCO 3
+        String[] bloco3 = {
+            "Depois de perceber que não há ninguém no campus, você sai em"
+            + " busca de artefatos para se\ndefender, então encontra o "
+            + "almoxarifado dos materiais de jardinagem do campus da sua"
+            + "\nuniversidade, onde você encontra alguns objetos",
+            "Com a "+arma+" em mãos, você percorre o campus da universidade"
+            + " em busca de outras pessoas,\nmas principalmente, de alimentos"
+            + " para sua sobrevivência. Com sorte, você avista um ser\nhumano,"
+            + " mas corre o risco dele estar infectado.",
+            "Então, você sai em busca de alimentos em algum lugar do campus."
+            + " Ao caminhar, você percebe\num grupo de 5 zumbis que se"
+            + " aproximam muito rapidamente de seu local.",
+            "Após se juntarem, vocês saem pelo campus em busca de alimentos."
+            + "\nAo caminharem, vocês percebem um grupo de 5 zumbis que se"
+            + " aproximam muito rapidamente\nde onde vocês estão.",
+            "Você consegue fugir. Após rodar por horas pelo campus da "
+            + "universidade em busca de alimentos,\nvocê finalmente encontra"
+            + " uma lanchonete, onde encontra comida o suficiente para"
+            + " passar\nalguns dias.",
+            
+        };
         //BLOCO 34
+        
+        String[] bloco34 = {
+            "Ao encontrar comida na lanchonete, você com sorte também"
+            + " encontra um rádio de pilha, que\ndecide levar consigo. Você"
+            + " ouve no rádio que há um grupo grande de pessoas reunidas na"
+            + "\ncidade, porém não sabe o local exato.",
+            "Logo ao sair do campus você avista um grupo de zumbis e sai"
+            + " correndo. Você consegue fugir,\ne mesmo após entender o"
+            + " perigo você sai em busca do grupo de pessoas que ouviu no"
+            + " rádio, porém\nno meio do caminho você é surpreendido por um"
+            + " grupo grande de zumbis que te matam."
+        };
         //BLOCO 4
+        String[] bloco4 = {
+            "Você decidiu ficar no campus por ser mais seguro que a cidade."
+            + " Agora, a busca é por um\nabrigo, já que o alojamento"
+            + " universitário está infestado de zumbis. Após horas andando"
+            + " e\navaliando os prédios, uma pessoa aparece em apuros. "
+            + "Ao se aproximar dela, nota que ela\nteve um pé amputado. "
+            + "Ciente do perigo, você a ajuda, carregando-a pelos ombros."
+            + "\nApós um tempo caminhando para um local seguro, percebem que"
+            + " atrás de vocês há 3 zumbis",
+            "Com sorte e livre dos zumbis, vocês encontram um prédio com as"
+            + " portas abertas, então\ndecidem entrar para descansar um pouco."
+            + " Ao entrar, percorrem um corredor e se deparam\ncom um "
+            + "laboratório de química vandalizado que exala um cheiro forte",
+            "Seguindo seu caminho pelo campus, vocês avistam um prédio mais"
+            + " afastado e isolado.\nEle parece ser de algum departamento de"
+            + " pesquisa médica, pois tem uma imagem de uma\nestrutura de DNA"
+            + " na entrada. Por fora parece vazio e silencioso, "
+            + "então vocês entram"
+        };
         
         //BLOCO 5
         String[] bloco5 = {
@@ -249,9 +306,27 @@ public class apocalipse {
         
        
         //BLOCO3
+        String[][] situacaoBloco3 = {
+/*0*/       {"Você escolhe: ", "a) uma enxada", "b) uma tesoura de poda",
+            "c) uma pá", "d) uma foice"},
+/*1*/       {"Você: ", "a) tenta contato", "b) segue seu caminho sozinho"},
+/*1*/       {"Você: ", "a) tenta enfrentá-los", "b) foge"}
+        };
         //BLOCO34
+        String[][] situacaoBloco34 = {
+/*0*/       {"Você: ", "a) Arrisca sair do campus para procurar este grupo"
+            + " de pessoas", "b) Fica no campus"}
+        };
+                
         //BLOCO4
-
+        String[][] situacaoBloco4 = {
+ /*0*/      {"Impossibilitados de correr por conta da pessoa ferida, vocês: ",
+            "a) abandonam a pessoa para se livrarem dos zumbis", "b) "
+            + "pegam ela no colo e correm"},
+ /*1*/      {"Vocês: ", "a) vão embora com medo de passarem mal com o cheiro",
+            "b) protegem o rosto e entram no laboratório"}
+            
+        };
         String[][] situacaoBloco5 = {
 /*sit0*/    {"A primeira coisa que você faz é checar se tem água, ao "
 /*aleat*/   + "entrar no banheiro ", "você é surpreendido\npor um zumbi que te"
@@ -281,16 +356,27 @@ public class apocalipse {
         
         
         //TEXTOS EXTRA BLOCO E SITUAÇÃO
+        
+        //texto de alternativa bloco 3, situação 1
+        String textoBloco3A = "Como ele não estava infectado, vocês se juntam";
+        String textoBloco3B = "A pessoa estava infectada, portanto você a"
+                + " matou com ";
+        String morteSit2Amigo = "Vocês não deram conta de matar todos os"
+                + " zumbis. Eles te atacaram e vocês morreram.";
+        String morteSit2 = "Você não deu conta de matar todos os zumbis"
+                + " rapidamente, então eles te atacaram e você morreu.";
+
         //texto de alternativa bloco 5, situação 1
         String textoSituacao2 = "Ao abrir, é pego de surpresa por um zumbi, que"
             + " tenta te atacar mas você consegue rapidamente \nacertar ele na "
-            + "cabeça com "+arma+", apesar de sair com um pequeno ferimento."
+            + "cabeça com a "+arma+", apesar de sair com um pequeno ferimento."
             + "\nEssa sala tem uma janela, que você cobre com as carteiras.\n"
             + "Depois de proteger as janelas, você acha no canto da sala um kit"
             + " de primeiros socorros para \ncuidar do ferimento, mas nada "
             + "além disso. \nPor precaução, acha melhor deixar a porta "
             + "fechada e fazer uma barricada para impedir a \npassagem por "
             + "ela também.";
+        
         //texto de morte do bloco 6
         String morteEspecial = "No caminho, uma horda de zumbis te pega de "
                 + "surpresa e te devora. Infelizmente você"
@@ -313,10 +399,6 @@ public class apocalipse {
             + " surpreendido ao chegar na sala por um senhor aparentemente"
             + " deformado, sem um dos braços\nque te chama pedindo ajuda, e"
             + " o ataca em seguida"};
-        
-
-
-        
         
         //MONTAGEM DOS BLOCOS
         do{ //deNovoJogo
@@ -341,8 +423,7 @@ public class apocalipse {
                             imprimirSituacao(1, situacaoBloco1);
                             op = entrada();
                             if(op == 'a'){
-                                //próximo blocoo
-                                System.out.println("prox bloco");
+                                //próximo bloco
                                 break;
                             } else{
                                 saude = 0;
@@ -381,18 +462,15 @@ public class apocalipse {
                     }
                 }
             }while(!deNovo);
-            
-            System.out.println("Status: ");
-            System.out.println(saude);
-            System.out.println(pontos);
-            
+      
             //deseja continuar?            
             if(morreu == false){
                 num = proximoBloco();
                 if(num == 1){ //para rodar o próximo bloco
                     deNovo = false;
+                    imprimirStatus(saude, arma);
                 } else{
-                    //status
+                    imprimirStatus(saude, arma);
                     break;
                 }
             }
@@ -443,24 +521,173 @@ public class apocalipse {
                 
             }
             
-            System.out.println("Status: ");
-            System.out.println(saude);
-            System.out.println(pontos);
+
+            //deseja continuar?            
+            if(morreu == false){
+                num = proximoBloco();
+                if(num == 1){ //para rodar o próximo bloco
+                    deNovo = false;
+                    imprimirStatus(saude, arma);
+                } else{
+                    imprimirStatus(saude, arma);
+                    break;
+                }
+            }
+            
+            //BLOCO3
+            saudeT = saude; pontosT = pontos; //checkpoint
+            while(!deNovo){
+                imprimirBreak();
+                deNovo = true; 
+                if(morreu){
+                    saude = saudeT;
+                    pontos = pontosT;
+                    morreu = false;
+                }
+                
+                for(int i=0;i<bloco3.length;i++){
+                    System.out.println(bloco3[i]);
+                    System.out.println();
+                    if(i==0){
+                        imprimirSituacao(0,situacaoBloco3);
+                        op = entrada();
+                        switch (op) {
+                            case 'a':
+                                    arma = "enxada";
+                                    break;
+                            case 'b':
+                                    arma = "tesoura de poda";
+                                    break;
+                            case 'c':
+                                    arma = "pá";
+                                    break;
+                            case 'd':
+                                    arma = "foice";
+                                    break;
+                            default:
+                                    System.out.println("Arma inválida.");
+                        }
+                    }
+                    if(i==1){
+                        imprimirSituacao(1,situacaoBloco3);
+                        num = geradorAleatorio(2);
+                        if(num ==0){
+                            System.out.println(textoBloco3A);
+                            amigo = true;
+                        } else{
+                            System.out.println(textoBloco3B);
+                        }
+                    }
+                    if(i==2){
+                        imprimirSituacao(2, situacaoBloco3);
+                        op = entrada();
+                        if(op == 'a' && amigo){
+                            saude = 0;
+                            System.out.println(morteSit2Amigo);
+                            morreu = true;
+                            deNovo = deNovo();
+                            break;
+                        } else if(op=='a' && !amigo){
+                            saude = 0;
+                            System.out.println(morteSit2);
+                            morreu = true;
+                            deNovo = deNovo();
+                        }
+                    }
+                }
+            }
             
             //deseja continuar?            
             if(morreu == false){
                 num = proximoBloco();
                 if(num == 1){ //para rodar o próximo bloco
                     deNovo = false;
-                 } else{
+                    imprimirStatus(saude, arma);
+                } else{
+                    imprimirStatus(saude, arma);
+                    break;
+                }
+            }
+            //BLOCO34
+            saudeT = saude; pontosT = pontos; //checkpoint
+            while(!deNovo){
+                imprimirBreak();
+                deNovo = true;
+                if(morreu){
+                    saude = saudeT;
+                    pontos = pontosT;
+                    morreu = false;
+                }
+                for(int i=0;i<bloco34.length;i++){
+                    System.out.println(bloco34[0]);
+                    System.out.println();
+                    imprimirSituacao(0,situacaoBloco34);
+                    op=entrada();
+                    if(op == 'a'){
+                        System.out.println(bloco34[1]);
+                        saude = 0;
+                        morreu = true;
+                        deNovo = deNovo();
+                    } else{
+                        break;
+                    }
+                }
+            }
+            //deseja continuar?            
+            if(morreu == false){
+                num = proximoBloco();
+                if(num == 1){ //para rodar o próximo bloco
+                    deNovo = false;
+                    imprimirStatus(saude, arma);
+                } else{
+                    imprimirStatus(saude, arma);
                     break;
                 }
             }
             
-            //BLOCO3
-            //BLOCO34
             //BLOCO4
-           
+            saudeT = saude; pontosT = pontos; //checkpoint
+            while(!deNovo){
+                imprimirBreak();
+                deNovo = true;
+                if(morreu){
+                    saude = saudeT;
+                    pontos = pontosT;
+                    morreu = false;
+                }
+                for(int i=0;i<bloco4.length;i++){
+                    System.out.println(bloco4[i]);
+                    System.out.println();
+                    if(i==0){
+                        imprimirSituacao(0,situacaoBloco4);
+                        op = entrada();
+                        if(op == 'a'){
+                            pontos++;
+                        }
+                    }
+                    if(i==1){
+                        imprimirSituacao(1, situacaoBloco4);
+                        op = entrada();
+                        if(op == 'b'){
+                            pontos++;
+                        }
+                    }
+                }
+            }
+
+            //deseja continuar?            
+            if(morreu == false){
+                num = proximoBloco();
+                if(num == 1){ //para rodar o próximo bloco
+                    deNovo = false;
+                    imprimirStatus(saude, arma);
+                } else{
+                    imprimirStatus(saude, arma);
+                    break;
+                }
+            }
+            
+            
             //BLOCO 5
             saudeT = saude; pontosT = pontos; //checkpoint
             while(!deNovo){ 
@@ -536,18 +763,15 @@ public class apocalipse {
                     }
                 }
             } //fim bloco 5
-            
 
-            System.out.println("Status: ");
-            System.out.println(saude);
-            System.out.println(pontos);
-            
             //deseja continuar?            
             if(morreu == false){
                 num = proximoBloco();
                 if(num == 1){ //para rodar o próximo bloco
                     deNovo = false;
+                    imprimirStatus(saude, arma);
                 } else{
+                    imprimirStatus(saude, arma);
                     break;
                 }
             }
@@ -555,7 +779,7 @@ public class apocalipse {
             //bloco 6 (não repete)
             while(!deNovo){
                 imprimirBreak();
-    /*fica*/    if(pontos >= 3){
+    /*fica*/    if(pontos >= 4){
                     for(int i=0;i<6;i++){
                         System.out.println(bloco6[i]);
                         System.out.println();
@@ -573,8 +797,8 @@ public class apocalipse {
                         System.out.println(bloco6[i]);
                         System.out.println();
                         if(i==7){
-                            saude--;
-                            System.out.println("    -1 ponto de saúde");
+                            saude-=2;
+                            System.out.println("    -2 ponto de saúde");
                             System.out.println();
                             if(saude <= 0){
                                 imprimirMorte();
